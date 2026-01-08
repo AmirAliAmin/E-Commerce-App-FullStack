45dc1ab4a850c6fa2be248d15d88562886d1fcd7
AmirAliAmin <aliaminamir77@gmail.com>
2026-01-08 15:03:09 +0500

    mixed navbar ,improve it if at home the prduct li show if not at home then home li show

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -1,13 +1,23 @@
-import React, { useContext} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { RiMenu2Line } from "react-icons/ri";
 import { FaAngleDown } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IoRocketSharp } from "react-icons/io5";
 import { AppContext } from "../../context/AppContext";
 
 function Navbar({ setBoxOpen, boxOpen }) {
-  const {categoryData} = useContext(AppContext)
- 
+  const { categoryData } = useContext(AppContext);
+  const [homeNav, setHomeNav] = useState("Home");
+
+  const location = useLocation();
+  const navigate = useNavigate("")
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setHomeNav("Products");
+    } else{
+      setHomeNav("Home");
+    }
+  }, [location.pathname]);
 
   return (
     <nav className="py-2 border-b border-gray-200 shadow-lg relative">
@@ -25,13 +35,15 @@ function Navbar({ setBoxOpen, boxOpen }) {
         <div className="col_2 lg:w-[70%]  xl:w-[60%] overflow-x-auto overflow-y-scroll lg:overflow-visible no-scroll ">
           <ul className="flex items-center  whitespace-nowrap no-scroll gap-5 text-gray-700 ">
             <li className="list-none">
-               <Link
-                  to={`/`}
-                  className="link text-[16px] font-medium py-2 block"
-                  onClick={()=>scrollTo(0,0)}
-                >Home</Link>
+              <Link
+                to={homeNav === "Home" ?`/` : `/products`}
+                className="link text-[16px] font-medium py-2 block"
+                onClick={() => scrollTo(0, 0)}
+              >
+                {homeNav}
+              </Link>
             </li>
-            {categoryData?.slice(0,8)?.map((parent) => (
+            {categoryData?.slice(0, 8)?.map((parent) => (
               <li className="list-none relative group" key={parent._id}>
                 <Link
                   to={`/products?catId=${parent._id}`}
@@ -42,11 +54,14 @@ function Navbar({ setBoxOpen, boxOpen }) {
                 <div className="absolute top-full left-0 hidden group-hover:block bg-white h-20  pt-2 z-500 shadow-2xl rounded-lg">
                   <ul className="">
                     {parent.children?.map((child) => (
-                      <Link to={`/products?subcatId=${child._id}`} key={child._id} className="px-4 py-2 text-gray-800  border-b border-gray-100 link">
+                      <Link
+                        to={`/products?subcatId=${child._id}`}
+                        key={child._id}
+                        className="px-4 py-2 text-gray-800  border-b border-gray-100 link"
+                      >
                         {child.name}
                       </Link>
                     ))}
-                   
                   </ul>
                 </div>
               </li>
